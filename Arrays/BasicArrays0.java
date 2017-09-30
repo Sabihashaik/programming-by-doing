f68ef28925de7798755801c1c11829c1c4ab014f
Sabiha Shaik <sabihaabc1@gmail.com>
2017-09-30 21:41:40 +0400

    Create BasicArrays0.java

---
+++ Arrays/BasicArrays0.java
@@ -0,0 +1,29 @@
+public class BasicArrays0 {
+	public static void main(String[] args) {
+		int A[]=new int[10];
+		
+		//Inserting elements
+		A[0]=-113;
+		A[1]=-113;
+		A[2]=-113;
+		A[3]=-113;
+		A[4]=-113;
+		A[5]=-113;
+		A[6]=-113;
+		A[7]=-113;
+		A[8]=-113;
+		A[9]=-113;
+		
+		//Printing Elements
+		System.out.println("Slot "+"0"+" contains a "+A[0]);
+		System.out.println("Slot "+"1"+" contains a "+A[1]);
+		System.out.println("Slot "+"2"+" contains a "+A[2]);
+		System.out.println("Slot "+"3"+" contains a "+A[3]);
+		System.out.println("Slot "+"4"+" contains a "+A[4]);
+		System.out.println("Slot "+"5"+" contains a "+A[5]);
+		System.out.println("Slot "+"6"+" contains a "+A[6]);
+		System.out.println("Slot "+"7"+" contains a "+A[7]);
+		System.out.println("Slot "+"8"+" contains a "+A[8]);
+		System.out.println("Slot "+"9"+" contains a "+A[9]);
+	}
+}

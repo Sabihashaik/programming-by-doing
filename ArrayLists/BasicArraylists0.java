1c13a1304b0ee5a1f7584e73a7995088920ea234
Sabiha Shaik <sabihaabc1@gmail.com>
2017-09-30 21:47:37 +0400

    Create BasicArraylists0.java

---
+++ ArrayLists/BasicArraylists0.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class BasicArraylists0 {
+	public static void main(String[] args) {
+		ArrayList<Integer> arr= new ArrayList<Integer>();
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		arr.add(-113);
+		
+		//Printing the output
+		System.out.println("Slot "+"0"+" contains a "+arr.get(0));
+		System.out.println("Slot "+"1"+" contains a "+arr.get(1));
+		System.out.println("Slot "+"2"+" contains a "+arr.get(2));
+		System.out.println("Slot "+"3"+" contains a "+arr.get(3));
+		System.out.println("Slot "+"4"+" contains a "+arr.get(4));
+		System.out.println("Slot "+"5"+" contains a "+arr.get(5));
+		System.out.println("Slot "+"6"+" contains a "+arr.get(6));
+		System.out.println("Slot "+"7"+" contains a "+arr.get(7));
+		System.out.println("Slot "+"8"+" contains a "+arr.get(8));
+		System.out.println("Slot "+"9"+" contains a "+arr.get(9));
+		
+		
+	}
+}
